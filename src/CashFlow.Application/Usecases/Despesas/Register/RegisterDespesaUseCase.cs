7af18a83c7a529a14f52cc6db05a799230f368eb
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2025-05-06 18:22:02 -0400

    Adicionar validação a RegisterDespesaUseCase
    Aprimoramento de RegisterDespesaUseCase com a adição de um novo método Validate para verificar o objeto RequestRegisterDespesaJson em busca de campos obrigatórios e valores válidos. Atualização do método Execute para chamar Validate antes de retornar a resposta. Também foram incluídas diretivas using adicionais para enumerações e respostas.

--- src/CashFlow.Application/Usecases/Despesas/Register/RegisterDespesaUseCase.cs
+++ src/CashFlow.Application/Usecases/Despesas/Register/RegisterDespesaUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.Usecases.Despesas.Register;
@@ -7,11 +8,32 @@ public class RegisterDespesaUseCase
 
     public ResponseRegisterDespesaJson Execute(RequestRegisterDespesaJson req)
     {
+
+        Validate(req);
         return new ResponseRegisterDespesaJson
         {
             Title = req.Title,
             
             
         };
+    }
+
+    //funcao de validaçõs
+    private void Validate(RequestRegisterDespesaJson req)
+    {
+        if (string.IsNullOrEmpty(req.Title))
+            throw new ArgumentException("O título é obrigatório.");
+
+        if (req.Valor < 0)
+            throw new ArgumentException("O valor deve ser maior que zero.");
+
+        if (req.Data == DateTime.MinValue)
+            throw new ArgumentException("A data é obrigatória.");
+
+       
+
+        var pagamentoValido = Enum.IsDefined(typeof(PagamentoType), req.Pagemento);
+        if (!pagamentoValido)
+            throw new ArgumentException("O tipo de pagamento é inválido.");
     }
 }
