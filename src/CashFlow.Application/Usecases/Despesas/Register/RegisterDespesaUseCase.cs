fe694e79421cc96133d217eafebddf4a525e353d
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2025-05-06 17:57:49 -0400

    Adicionar RegisterDespesaUseCase para registro de despesas
    - Criada uma nova estrutura de pastas para casos de uso "Despesas" em `CashFlow.Application.csproj`.
    - Implementada a classe `RegisterDespesaUseCase` em `RegisterDespesaUseCase.cs` para lidar com o registro de despesas.
    - O método `Execute` processa `RequestRegisterDespesaJson` e retorna um `ResponseRegisterDespesaJson` com o título da solicitação.

---
+++ src/CashFlow.Application/Usecases/Despesas/Register/RegisterDespesaUseCase.cs
@@ -0,0 +1,17 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.Usecases.Despesas.Register;
+public class RegisterDespesaUseCase
+{
+
+    public ResponseRegisterDespesaJson Execute(RequestRegisterDespesaJson req)
+    {
+        return new ResponseRegisterDespesaJson
+        {
+            Title = req.Title,
+            
+            
+        };
+    }
+}
